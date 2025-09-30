14dc5af4ef054169b75e35f4fdffd5354022750c
OneProg <jade.weirdo@gmail.com>
2025-09-30 17:51:22 +0300

    Ai service!

--- meowbot-ai/src/main.rs
+++ meowbot-ai/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenvy::dotenv;
+use meowbot_proto::generated::ai::ai_server::AiServer;
+use tonic::transport::Server;
+
+use crate::ollama::Ollama;
+
+mod ollama;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let ai_addr = env::var("AI_ADDR").expect("AI_ADDR mus be set!");
+    let ollama = Ollama;
+
+    Server::builder()
+        .add_service(AiServer::new(ollama))
+        .serve(ai_addr.parse()?)
+        .await?;
+
+    Ok(())
 }
---
+++ meowbot-ai/src/ollama.rs
@@ -0,0 +1,67 @@
+use meowbot_proto::generated::ai::{AiRequest, AiResponse, ai_server::Ai};
+use ollama_rs::generation::completion::request::GenerationRequest;
+use regex::Regex;
+use tonic::{Request, Response, Status};
+
+use string_format::string_format;
+
+pub struct Ollama;
+
+impl Ollama {
+    fn remove_reasoning(text: &str) -> String {
+        let re = Regex::new(r"<think\b[^>]*>[\s\S]*?</think>").expect("Failed to remove reasoning");
+        re.replace_all(text, "").into()
+    }
+}
+
+#[cfg(test)]
+mod remove_reasoning_tests {
+    use crate::ollama::Ollama;
+
+    #[test]
+    fn test_remove_reasoning_no_newline() {
+        let text = "<think>My thoughts...</think>Hello world!";
+        assert_eq!(Ollama::remove_reasoning(text), "Hello world!");
+        let text = "<think> My thoughts...</think> Hello world!";
+        assert_eq!(Ollama::remove_reasoning(text), " Hello world!");
+    }
+
+    #[test]
+    fn test_remove_reasoning_with_newline() {
+        let text = "<think>My thoughts...\nMore...</think>Hello world!";
+        assert_eq!(Ollama::remove_reasoning(text), "Hello world!");
+        let text =
+            "<think>My thoughts...\nMore...\nAND MORE!\n\n\n\nThoughts...</think>Hello world!";
+        assert_eq!(Ollama::remove_reasoning(text), "Hello world!");
+    }
+}
+
+#[tonic::async_trait]
+impl Ai for Ollama {
+    async fn get_response(
+        &self,
+        request: Request<AiRequest>,
+    ) -> Result<Response<AiResponse>, Status> {
+        let request = request.get_ref().to_owned();
+
+        let weather = request.weather;
+        let prompt = request.prompt;
+
+        let ollama = ollama_rs::Ollama::default();
+        let response = ollama
+            .generate(GenerationRequest::new(
+                "qwen3:30b".into(),
+                string_format(prompt, weather),
+            ))
+            .await
+            .expect("Failed to get AI response")
+            .response;
+        let fmt_response = Self::remove_reasoning(&response);
+
+        let response = AiResponse {
+            response: fmt_response,
+        };
+
+        Ok(Response::new(response))
+    }
+}
