0dafe11172256ab8180f7860ffb8231766708590
OneProg <jade.weirdo@gmail.com>
2025-09-30 13:25:21 +0300

    Proto is a cdylib now

--- meowbot-proto/build.rs
+++ meowbot-proto/build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
-        .out_dir("src/gen")
+        .out_dir("src/generated")
         .compile_protos(
             &[
                 "protos/models.proto",
---
+++ meowbot-proto/src/generated.rs
@@ -0,0 +1,4 @@
+pub mod ai;
+pub mod db;
+pub mod models;
+pub mod weather;
